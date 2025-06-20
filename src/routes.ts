146ddf9efe47d8d6f89611e8a2152aeb43f2ab45
YanGraim <yangraim@gmail.com>
2025-06-20 10:32:22 -0300

    query params

--- src/routes.ts
+++ src/routes.ts
@@ -3,10 +3,23 @@ import { Request, Response, Router } from "express";
 
 const router = Router();
 
+// Route Params /tarefas/2
+// Request Body { "nome": "Comprar Pao", "usuario": 123 }
+
 //Exemplo: http://localhost:3333/tarefas
+// router.get("/tarefas", (req: Request, res: Response) => {
+//     // res.send("My first api");
+//     res.json({ message: "My first api" });
+// });
+
+// export { router }
+
+
+// Query Params ?nome=Comprar Pao
 router.get("/tarefas", (req: Request, res: Response) => {
-    // res.send("My first api");
-    res.json({ message: "My first api" });
+    const nome = req.query.nome;
+
+    res.json({ tarefas: nome });
 });
 
 export { router }
